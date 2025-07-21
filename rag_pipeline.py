d71e933aacbee8d107bad323e263c944bbf26eaf
anveshasingh12 <anveshasingh9283@gmail.com>
2025-07-21 18:11:15 +0530

    Updated rag_pipeline.py and added .gitignore to exclude cache/files

--- rag_pipeline.py
+++ rag_pipeline.py
@@ -1,8 +1,7 @@
 from langchain.chains import RetrievalQA
 from langchain.vectorstores import FAISS
-from langchain_openai import ChatOpenAI
 from langchain.embeddings import HuggingFaceEmbeddings
-
+from langchain_groq import ChatGroq
 
 from dotenv import load_dotenv
 import os
@@ -11,10 +10,15 @@ load_dotenv()
 
 def load_qa_chain():
     embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
-    db = FAISS.load_local("faiss_index", embeddings)
+    db = FAISS.load_local("faiss_index", embeddings, allow_dangerous_deserialization=True)
+
     retriever = db.as_retriever(search_kwargs={"k": 3})
 
-    llm = ChatOpenAI(temperature=0.2, model="gpt-3.5-turbo", api_key=os.getenv("OPENAI_API_KEY"))
+    llm = ChatGroq(
+        temperature=0.2,
+        groq_api_key=os.getenv("GROQ_API_KEY"),
+        model_name="llama3-70b-8192"  # ✅ Use a supported model
+    )
+
     qa_chain = RetrievalQA.from_chain_type(llm=llm, retriever=retriever, return_source_documents=True)
     return qa_chain
-
